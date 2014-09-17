d0dcc944c08d9e952c172760c6dc4af3e71a356a
Sean Levin <slevin@yahoo-inc.com>
2014-09-17 10:20:25 -0400

    starting it off

---
+++ haskell/cis/h3/Lan.hs
@@ -0,0 +1,26 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Lan where
+
+import Log
+
+
+-- parseMessage "E 2 256 somethign happens here" -> ValidLM (LogMessgae (Error 2) 256 "something...")
+-- parseMessage "k wrong" -> InvalidLM
+parseMessage :: String -> MaybeLogMessage
+parseMessage str = let lst = words str in
+                    case lst of
+                     ("E":errNumStr:timeStr:rest) -> let errNum = readInt errNumStr;
+                                                         timeNum = readInt timeStr in
+                                                      case (errNum, timeNum) of
+                                                       (ValidInt eNum, ValidInt tNum) -> ValidLM (LogMessage (Error eNum) tNum (unwords rest))
+                                                       _ -> InvalidLM str
+                     ("I":timeStr:rest) -> let timeNum = readInt timeStr in
+                                            case timeNum of
+                                             ValidInt tNum -> ValidLM (LogMessage Info tNum (unwords rest))
+                                             _ -> InvalidLM str
+                     ("W":timeStr:rest) -> let timeNum = readInt timeStr in
+                                            case timeNum of
+                                             ValidInt tNum -> ValidLM (LogMessage Warning tNum (unwords rest))
+                                             _ -> InvalidLM str
+                     _ -> InvalidLM str
