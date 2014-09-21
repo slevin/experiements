d0e621f972d76477d0eb59b3a7d8ad59d7c74f9a
Sean Levin <slevin@yahoo-inc.com>
2014-09-21 10:11:00 -0400

    finish hw3

--- haskell/cis/h3/Lan.hs
+++ haskell/cis/h3/Lan.hs
@@ -4,6 +4,7 @@ module Lan where
 
 import Log
 import Data.List
+import Data.Char
 
 -- parseMessage "E 2 256 somethign happens here" -> ValidLM (LogMessgae (Error 2) 256 "something...")
 -- parseMessage "k wrong" -> InvalidLM
@@ -45,4 +46,21 @@ sortMessages :: [LogMessage] -> [LogMessage]
 sortMessages msgs = sortBy compareMsgs msgs
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong msgs@(LogMessage type time text) = filter
+whatWentWrong msgs = fmap (\(LogMessage _ _ s) -> s) $ sortMessages $ filter (\(LogMessage t _ _) -> case t of
+                                                                      (Error v) -> v >= 50
+                                                                      _ -> False)
+                                                              msgs
+
+strupper :: String -> String
+strupper s = map toUpper s
+
+messagesAbout :: String -> [LogMessage] -> [LogMessage]
+messagesAbout s msgs = filter (\(LogMessage _ _ m) -> isInfixOf (strupper s) (strupper m)) msgs
+
+fltr :: String -> [LogMessage] -> [LogMessage]
+fltr str msgs = filter (\(LogMessage t _ m) -> case (t,m) of
+                                                ((Error v), _) -> v >= 50
+                                                (_, m) -> isInfixOf (strupper str) (strupper m)) msgs
+
+whatWentWrongEnhanced :: String -> [LogMessage] -> [String]
+whatWentWrongEnhanced str msgs = fmap (\(LogMessage _ _ s) -> s) $ sortMessages $ fltr str msgs
