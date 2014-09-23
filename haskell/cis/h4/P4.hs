f4b63c0013f4bb9f23d58f36e71dac1b6dd0d0b3
Sean Levin <sean.levin@gmail.com>
2014-09-22 22:13:50 -0400

    some more

--- haskell/cis/h4/P4.hs
+++ haskell/cis/h4/P4.hs
@@ -4,11 +4,28 @@ import Data.List
 import Data.Char
 import Data.Maybe
 
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
 caps :: String -> Bool
-caps [] = False
-caps (x:_) = x == toUpper x
+caps s = fmap toUpper (safeHead s) == safeHead s && safeHead s /= Nothing
 
 allCaps :: [String] -> Bool
 allCaps msgs = and $ map caps msgs
 
---dropTrailingWhitespace :: String -> String
+
+
+dropTrailingWhitespace :: String -> String
+dropTrailingWhitespace str = dropWhileEnd isSpace str
+
+firstLetters :: [String] -> [Char]
+firstLetters strs = concat $ fmap (take 1) strs
+
+asList :: [String] -> String
+asList strs = "[" ++ (intercalate "," strs) ++ "]"
+
+{-
+
+
+-}
