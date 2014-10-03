41540a8b78ba815eb8a61397c8b77683e57a7925
Sean Levin <slevin@yahoo-inc.com>
2014-10-03 18:04:57 -0400

    generate starting square structure

--- elm/same.elm
+++ elm/same.elm
@@ -15,7 +15,6 @@ goal:
 - use signals
 
 check list:
-- draw 10 x 5 grid of squares same color with gaps
 - draw random colors
 - add some randomness to heights
 - respond to click
@@ -37,8 +36,10 @@ height = 250
 sq = 40
 sp = 2
 box color = filled color (square sq)
-offsetBL idxX idxY = move ((sq / 2) - (width / 2) + (sq * idxX) + (sp * idxX) , 
+offsetBL (idxX, idxY) = move ((sq / 2) - (width / 2) + (sq * idxX) + (sp * idxX) , 
                            (sq / 2) - (height / 2) + (sq * idxY) + (sp * idxY))
-main = collage width height [ offsetBL 0 0 (box red)
-                            , offsetBL 0 1 (box red)
-                            , offsetBL 1 0 (box red)]
+
+pairs = map (\x -> map (\y -> (x, y)) [0..4]) [0..9]
+allPairs = concat pairs
+squares = map (\pair -> offsetBL pair (box red)) allPairs
+main = collage width height squares
