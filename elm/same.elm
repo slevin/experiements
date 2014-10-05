43865dd550765e790dcb2dd8e6501dba2db76c49
Sean Levin <sean.levin@gmail.com>
2014-10-05 10:32:56 -0400

    have random colors being selected
    made random signal move to the top and passed in a random list to build
    the model from so all the model stuff stays clean from signal code which
    seems like a nice way to do it. It makes it explicit that we've made
    things more complicated now.

--- elm/same.elm
+++ elm/same.elm
@@ -15,82 +15,77 @@ goal:
 - use signals
 
 check list:
-- draw random colors
 - respond to click
 - check neighbors
 - remove those guys if necessary
 - redraw squares
 
 small steps:
-assign random colors over those
+
 
 limit size to fit area
 
-how do I get random between three colors
-
-now we need to take a random 1 of 4 colors
-as I'm building up the positions and create some sort of record
-then I can use the color to sort out box
 
 then I need to set up some response to mouse tap
 
 which can somehow build up a list of things that will disappear
 then need to remove those from the list of everything and rerender
 the state crazy
+
+my colors are biased towards 1 because I'm not collecting between each one
+I should to 0 to 3 and round down instead
 -}
 
-import Random (range)
-
+import Random (floatList)
+import Array
 
 width = 500
 height = 250
 sq = 40
 sp = 2
 
-type BoxModel = { pos:(Int,Int), color:Color }
+type BoxModel = { x:Int, y:Int, color:Color }
 type RandomList = [Float]
+type RandomArray = Array.Array Float
 
 box color = filled color (square sq)
 offsetBL (idxX, idxY) = move ((sq / 2) - (width / 2) + (sq * idxX) + (sp * idxX) , 
                            (sq / 2) - (height / 2) + (sq * idxY) + (sp * idxY))
-num : Int -> Signal Int
-num x = range 1 5 (constant x)
 
 numarray : RandomList -> [Int]
 numarray fs = zipWith (\x y -> floor <| x * 5) fs [0..9]
 
-idx2Pos : [Int] -> [[(Int, Int)]]
-idx2Pos arr = indexedMap (\idx num -> map (\y -> (idx, y)) [0..num]) arr
+pos2Color : RandomArray -> (Int, Int) -> Color
+pos2Color arr (x, y) = let n = Array.getOrElse 0 (x*9 + y) arr in
+                           num2Color n
+                           
+pos2Box : RandomArray -> Int -> Int -> BoxModel
+pos2Box ars x y = BoxModel x y (pos2Color ars (x, y))
 
-allPairs : RandomList -> [(Int, Int)]
-allPairs fs = concat <| idx2Pos <| numarray fs
+columns2Boxes : RandomList -> [Int] -> [[BoxModel]]
+columns2Boxes rs arr = let ars = Array.fromList rs in
+                           indexedMap (\idx num -> map (\y -> (pos2Box ars idx y)) [0..num]) arr
 
-num2Color : Int -> Color
-num2Color i = if | i == 0 -> red
-                 | i == 1 -> green
+allBoxes : RandomList -> [BoxModel]
+allBoxes rs = concat <| columns2Boxes rs <| numarray rs
+
+num2Color : Float -> Color
+num2Color f = let n = round <| f * 2 in
+              if | n == 0 -> red
+                 | n == 1 -> green
                  | otherwise -> blue
 
-randomColor : a -> Signal Color
-randomColor i = lift num2Color (range 0 2 (constant i))
-
-pair2Box : (Int,Int) -> Signal BoxModel
-pair2Box pair = lift2 BoxModel (constant pair) (randomColor pair)
-
-allBoxes' : Signal [Signal BoxModel]
-allBoxes' = lift2 map (constant pair2Box) allPairs
-
-{-
-allBoxes : Signal [BoxModel]
-allBoxes = combine allBoxes'
-
-
 box2Square : BoxModel -> Form
-box2Square b = offsetBL (toFloat <| fst b.pos, toFloat <| snd b.pos) (box b.color)
+box2Square b = offsetBL (toFloat <| b.x, toFloat <| b.y) (box b.color)
 
-allSquares : Signal [Form]
-allSquares = lift2 map (constant box2Square) allBoxes
+allSquares : RandomList -> [Form]
+allSquares rs = map box2Square <| allBoxes rs
 
-main = lift3 collage (constant width) (constant height) allSquares
+randomSignal : Signal [Float]
+randomSignal = floatList (constant 100)
 
+main = let 
+           all = lift allSquares randomSignal 
+       in
+           lift3 collage (constant width) (constant height) all
 
--}
