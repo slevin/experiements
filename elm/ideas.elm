ccc6920b71fedd050060c2dc16967000d814a373
Sean Levin <sean.levin@gmail.com>
2014-10-09 23:10:39 -0400

    ability to diff lists of BoxModel

--- elm/ideas.elm
+++ elm/ideas.elm
@@ -8,6 +8,11 @@ working back I have a set of positions, I need a set of positions to delete
 I need to remove all the set from the total set
 
 1 given list of box and given second list of box create new box - old box
+
+for each item in first list 
+if its in second list don't include it
+
+so box in boxlist
 -}
 
 import Set (diff, fromList, toList)
@@ -21,9 +26,22 @@ b3 = BoxModel 2 0 red
 b4 = BoxModel 0 1 red
 
 s1 = [b1, b2, b3]
-s2 = [b2, b3]
+s2 = [b1, b3]
 
-s3 : [BoxModel] -> [BoxModel] -> [BoxModel]
-s3 total minus = (fromList total) `diff` (fromList minus)
+boxEquals : BoxModel -> BoxModel -> Bool
+boxEquals b1 b2 = b1.x == b2.x && b1.y == b2.y
+
+boxInBoxList : BoxModel -> [BoxModel] -> Bool
+boxInBoxList box list = case list of
+                          [] -> False
+                          (b::bs) -> if | boxEquals box b -> True
+                                        | otherwise -> boxInBoxList box bs
+
+boxListDiff : [BoxModel] -> [BoxModel] -> [BoxModel]
+boxListDiff group rest = filter (\x -> (boxInBoxList x rest) == False) group
+
+s3 = boxListDiff s1 s2
+
+main = leftAligned <| toText <| show s3
 
 --main = toText "hello"
