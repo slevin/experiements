992efa996d6b2beecdf0bc6f57d266c9e8d76ded
Sean Levin <sean.levin@gmail.com>
2014-10-22 22:38:45 -0400

    better tests to solve partition
    looking better but its not sorting properly

--- elm/slider-tests.elm
+++ elm/slider-tests.elm
@@ -5,35 +5,16 @@ import ElmTest.Assertion (..)
 import ElmTest.Runner.Element (..)
 import ElmTest.Test (..)
 
-{-
-assertEqualList : [comparable] -> [comparable] -> Assertion
-assertEqualList l1 l2 = assertEqual (sort l1) (sort l2)
-
-swapHorizontalTest = let 
-                         after = swap 8 start
-                     in
-                       equals after [[1,2,3],[4,5,6],[7,emptySquare,8]]
-
-swapVerticalTest = let 
-                       after = swap 6 start
-                   in
-                     equals after [[1,2,3],[4,5,emptySquare],[7,8,6]]
-
-findNeighborsTest = let found = findNeighbors emptySquare start
-                    in defaultTest <| assertEqualList found [8,6]
-
--}
-
+sq1 = {x=0,y=0,tile=Tile 1}
+sq2 = {x=1,y=0,tile=Tile 2}
+sq3 = {x=0,y=1,tile=Tile 3}            
+sq4 = {x=1,y=1,tile=EmptyTile}
+      
 starterListTest = let after = starterList 2 2
                   in equals after [[Tile 1, Tile 2],[Tile 3, EmptyTile]]
 
 starterBoardTest = let after = starterBoard 2 2
-                   in equals [{x=0,y=0,tile=Tile 1}
-                             ,{x=1,y=0,tile=Tile 2}
-                             ,{x=0,y=1,tile=Tile 3}
-                             ,{x=1,y=1,tile=EmptyTile}
-                             ] after
-
+                   in equals [sq1, sq2, sq3, sq4] after
 
 nextToEmptyTest1 = defaultTest <| assert <| nextToEmpty (Tile 8) start
 nextToEmptyTest2 = defaultTest <| assert <| nextToEmpty (Tile 6) start
@@ -51,12 +32,24 @@ neighborsTest4 = let n = neighbors (Tile 20) start
 
 swapWithEmptyTest = let after = swapWithEmpty (Tile 2) <| starterBoard 2 2
                     in
-                      equals [{x=0,y=0,tile=Tile 1}
+                      equals [sq1
                              ,{x=1,y=0,tile=EmptyTile}
-                             ,{x=0,y=1,tile=Tile 3}
+                             ,sq3
                              ,{x=1,y=1,tile=Tile 2}
                              ] after
 
+spbTest1 = let after = squarePartitionedByY sq1 []
+           in
+             equals [[sq1]] after
+
+spbTest2 = let after = squarePartitionedByY sq2 [[sq1]]
+           in
+             equals [[sq1, sq2]] after
+
+spbTest3 = let after = squarePartitionedByY sq3 [[sq1]]                    
+           in
+             equals [[sq1],[sq3]] after
+                    
 board2ListsTest = let after = starterBoard 2 2 |> board2Lists
                   in
                     equals [[{x=0,y=0,tile=Tile 1},{x=1,y=0,tile=Tile 2}],
@@ -74,7 +67,10 @@ s = suite "Board Functions"
     ,nextToEmptyTest2
     ,nextToEmptyTest3
     ,swapWithEmptyTest
-    ,board2ListsTest
+    ,spbTest1
+    ,spbTest2
+    ,spbTest3
+--    ,board2ListsTest
     ]
 
 main = runDisplay s
