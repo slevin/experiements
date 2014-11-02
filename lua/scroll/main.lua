7fbda6c7ef507713b09ac2f671adc642645ee02f
Sean Levin <sean.levin@gmail.com>
2014-11-02 11:16:04 -0500

    add an extension method to an object
    pretty cool to be able to hijack the existing oo mechanism to inject my
    own functionality into a class.

--- lua/scroll/main.lua
+++ lua/scroll/main.lua
@@ -1,12 +1,18 @@
 --[[
 
+is it possible to take an existing method
+  and get it take a version with some color struct?
+
 swiping card moves it left and right
 
+really it moves the set in a new direction then it should complete
+  to where it needs to
 
 
-list of cards means more of them
 
-flexible pixels (need to do scale)
+  list of cards means more of them
+
+  flexible pixels (need to do scale)
 
 
 --]]
@@ -33,46 +39,94 @@ local sq = display.newRoundedRect(display.contentCenterX,
                                   restHeight - (padding * 2), 20)
 
 
-sq:setFillColor(cardBlue.r, cardBlue.g, cardBlue.b)
+--sq:setFillColor(cardBlue.r, cardBlue.g, cardBlue.b)
+
+--[[
+  move sq offset by whatever amount
+--]]
+local function onObjectTouch(event)
+  if event.phase == "moved" then
+    local xoff = event.x - event.xStart
+    sq.x = sq.x + xoff
+  end
+end
+
+sq:addEventListener("touch", onObjectTouch)
+
+local oldIndex = getmetatable(sq).__index
+
+local function setFillColorC(self, colorTable)
+  self:setFillColor(colorTable.r, colorTable.g, colorTable.b)
+end
+
+local function myIndex(self, key)
+  print("in myindex with key" .. key)
+  if key == "setFillColorC" then
+    print("got it")
+    return setFillColorC
+  else
+    return oldIndex(self, key)
+  end
+end
+
+local temp = getmetatable(sq)
+temp.__index = myIndex
+setmetatable(sq, temp)
+
+print("something")
+print(myIndex)
+
+sq:setFillColorC(cardBlue)
+
+--sq:setFillColor(0.5,0.5,0.5)
 
 
+local function printAll(t)
+  local m = getmetatable(t)
+  for k,v in pairs(m) do
+    print(k)
+    print(v)
+  end
+end
+
+printAll(sq)
 
 
 --[[
--- ScrollView listener
-local function scrollListener( event )
+  -- ScrollView listener
+  local function scrollListener( event )
 
-    local phase = event.phase
-    if ( phase == "began" ) then print( "Scroll view was touched" )
-    elseif ( phase == "moved" ) then print( "Scroll view was moved" )
-    elseif ( phase == "ended" ) then print( "Scroll view was released" )
-    end
+  local phase = event.phase
+  if ( phase == "began" ) then print( "Scroll view was touched" )
+  elseif ( phase == "moved" ) then print( "Scroll view was moved" )
+  elseif ( phase == "ended" ) then print( "Scroll view was released" )
+  end
 
-    -- In the event a scroll limit is reached...
-    if ( event.limitReached ) then
-        if ( event.direction == "up" ) then print( "Reached top limit" )
-        elseif ( event.direction == "down" ) then print( "Reached bottom limit" )
-        elseif ( event.direction == "left" ) then print( "Reached left limit" )
-        elseif ( event.direction == "right" ) then print( "Reached right limit" )
-        end
-    end
+  -- In the event a scroll limit is reached...
+  if ( event.limitReached ) then
+  if ( event.direction == "up" ) then print( "Reached top limit" )
+  elseif ( event.direction == "down" ) then print( "Reached bottom limit" )
+  elseif ( event.direction == "left" ) then print( "Reached left limit" )
+  elseif ( event.direction == "right" ) then print( "Reached right limit" )
+  end
+  end
 
-    return true
-end
+  return true
+  end
 
--- Create the widget
-local scrollView = widget.newScrollView
-{
-    top = 100,
-    left = 10,
-    width = 300,
-    height = 400,
-    scrollWidth = 600,
-    scrollHeight = 800,
-    listener = scrollListener
-}
+  -- Create the widget
+  local scrollView = widget.newScrollView
+  {
+  top = 100,
+  left = 10,
+  width = 300,
+  height = 400,
+  scrollWidth = 600,
+  scrollHeight = 800,
+  listener = scrollListener
+  }
 
--- Create a image and insert it into the scroll view
-local background = display.newImageRect( "ziggy.jpg", 768, 1024 )
-scrollView:insert( background )
+  -- Create a image and insert it into the scroll view
+  local background = display.newImageRect( "ziggy.jpg", 768, 1024 )
+  scrollView:insert( background )
 --]]
