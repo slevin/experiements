6f04db66e0be9e157927c3e1f5f7cd074a314e04
Sean Levin <sean.levin@gmail.com>
2014-11-05 08:43:56 -0500

    operating over group of cards

--- lua/scroll/main.lua
+++ lua/scroll/main.lua
@@ -1,10 +1,7 @@
 local bottomHeight = 100
 local restHeight = display.contentHeight - bottomHeight
 local restCenterY = restHeight * 0.5
-
 local padding = 50
-
-local cardBlue = {r=0;g=0.65;b=0.97}
 
 -- background
 local bg = display.newRect(display.contentCenterX,
@@ -14,66 +11,84 @@ local bg = display.newRect(display.contentCenterX,
 
 bg:setFillColor(1)
 
-local indexOffset = 0
 local nextStop = display.contentWidth - (padding * 2)
 
+local function cardXAtOffset(indexOffset, moveOffset)
+  return display.contentCenterX +
+      ((nextStop + (padding * 0.5)) * indexOffset) +
+      moveOffset
+end
+
 local function cardAtOffset(indexOffset)
-  return display.newRoundedRect(display.contentCenterX +
-                                  ((nextStop + (padding * 0.5)) * indexOffset),
+  return display.newRoundedRect(cardXAtOffset(indexOffset, 0),
                                 restCenterY,
                                 display.contentWidth - (padding * 2),
                                 restHeight - (padding * 2),
                                 20)
 end
 
-
-
-
-local startOffsetX = 0
-local startX = 0
-
-local function onObjectTouch(event)
-  if event.phase == "began" then
-    startX = event.target.x
-    startOffsetX = startX - event.x
-  elseif event.phase == "moved" then
-    event.target.x = event.x + startOffsetX
-  elseif event.phase == "ended" or event.phase == "cancelled" then
-    print("stop phase " .. event.phase)
-    if event.target.x < (startX - (nextStop * 0.5)) then
-      transition.to(event.target, { x=(startX - nextStop), transition=easing.outExpo })
-    elseif event.target.x > (startX + (nextStop * 0.5)) then
-      transition.to(event.target, { x=(startX + nextStop), transition=easing.outExpo })
-    else
-      transition.to(event.target, { x=startX, transition=easing.outExpo })
-    end
-  else
-    print("whats this " .. event.phase)
-  end
-end
-
-
 local function printAll(t)
-  local m = getmetatable(t)
   for k,v in pairs(t) do
     print(k)
     print(v)
   end
 end
 
-local cards = {
-  {r=0;g=0.65;b=0.97},
-  {r=0.8;g=0.3;b=0.1},
-  {r=0.5;g=0.5;b=0.5}
-}
+local cardStack = {}
+cardStack.addCard = function(self, red, green, blue)
+  table.insert(cardStack, {r=red; g=green; b=blue})
+end
 
-local function renderCards(cards)
-  for i,c in ipairs(cards) do
-    print(c)
-    newCard = cardAtOffset(i - 2)
-    newCard:setFillColor(c.r, c.g, c.b)
-    newCard:addEventListener("touch", onObjectTouch)
+cardStack.positionEachXBy = function(self, moveOffset)
+  for i,c in ipairs(self) do
+    local indexOffset = i - 2
+    print(cardXAtOffset(indexOffset, moveOffset))
+    c.x = cardXAtOffset(indexOffset, moveOffset)
   end
 end
 
-renderCards(cards)
+
+cardStack:addCard(  0, 0.65, 0.97)
+cardStack:addCard(0.8, 0.3,  0.1)
+cardStack:addCard(0.5, 0.5,  0.5)
+
+
+local function cardTouchEventFactory(cardStack, card, indexOffset)
+  local startOffsetX
+  local startX
+  local function onObjectTouch(event)
+    if event.phase == "began" then
+      startX = event.target.x
+      startOffsetX = startX - event.x
+    elseif event.phase == "moved" then
+      cardStack:positionEachXBy(event.x - event.xStart)
+    elseif event.phase == "ended" or event.phase == "cancelled" then
+      --[[
+      print("stop phase " .. event.phase)
+      if event.target.x < (startX - (nextStop * 0.5)) then
+        transition.to(event.target, { x=(startX - nextStop), transition=easing.outExpo })
+      elseif event.target.x > (startX + (nextStop * 0.5)) then
+        transition.to(event.target, { x=(startX + nextStop), transition=easing.outExpo })
+      else
+        transition.to(event.target, { x=startX, transition=easing.outExpo })
+      end
+      --]]
+    else
+      print("whats this " .. event.phase)
+    end
+  end
+
+  return onObjectTouch
+end
+
+
+local function renderCards(cards)
+  for i,c in ipairs(cards) do
+    local indexOffset = i - 2
+    local newCard = cardAtOffset(indexOffset)
+    newCard:setFillColor(c.r, c.g, c.b)
+    newCard:addEventListener("touch", cardTouchEventFactory(cards, card, indexOffset))
+  end
+end
+
+renderCards(cardStack)
