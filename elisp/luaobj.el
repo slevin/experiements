0ec27f7bcf61382c89491c6b727b76dfbf2f1f7b
Sean Levin <sean.levin@gmail.com>
2015-01-14 22:02:59 -0500

    basic working core of lua tables
    needs test

--- elisp/luaobj.el
+++ elisp/luaobj.el
@@ -1,35 +1,6 @@
-;; what would a a lua style object system be like in elisp
-
-;; table is a hash or maybe some other data structure
-;; key/values
-;; some literal syntax or constructor
-;; if I do (call table method-key params)
-;;   will look up that
-
-;; has some __properties__ that can serve
-;; as more rich methods
-;; if __call__ is defined
-;;   I that gets called with all params if not defined
-;;
-;; there's a get and a set metamethod
-;; among others (that maybe I'd have to pass on)
-
-;; could even redefine some basic emacs objects
-;; in terms of these things so they are a bit extensible
-
-;; don't need polymorphic dispatch because anything can be
-;; anything from late binding
-
 ;; I could make it arraylike also by having a list pointer if the items are numeric?
 
 ;; there is the weak tables as well
-
-;; setting a value to nil is a remove
-
-;; setting also checks for set function first
-;; and gettting checks for get function
-;; and ltbl-call checks for that method in metatable?
-;;  or can do
 
 ;;  __add and __mul, there are __sub (for subtraction), __div (for division), __unm (for negation), and __pow (for exponentiation). We may define also the field __concat, to define a behavior for the concatenation operator.
 
@@ -37,36 +8,57 @@
 
 ;;  __tostring field in the set metatable:
 ;; __metatable field in the metatable, getmetatable will return the value of this field, whereas setmetatable will raise an error:
-;; I said earlier that, when we access an absent field in a table, the result is nil. This is true, but it is not the whole truth. Actually, such access triggers the interpreter to look for an __index metamethod: If there is no such method, as usually happens, then the access results in nil; otherwise, the metamethod will provide the result.
 
-;; When Lua detects that w does not have the requested field, but has a metatable with an __index field, Lua calls this __index metamethod, with arguments w (the table) and "width" (the absent key). The metamethod then indexes the prototype with the given key and returns the result.
-;; rawget and setting another ltbl as __index   http://www.lua.org/pil/13.4.1.html
 
-;; __newindex and rawset(t, k, v)
-
-;;I'm pretty sure there is a calling syntax like table()
-(ltbl-setmeta tbl metatbl)
-(ltbl-getmeta tbl)
-
+(defun ltblp (tbl)
+  (equal (car tbl) 'ltbl))
 
 (defun ltbl-new (&rest items)
-  (list #s(hash-table data items) nil))
+  (list 'ltbl #s(hash-table data items) nil))
 
 (defun ltbl-set (key value tbl)
+  ;; if key doesn't exist, check for and call'__newindex function
+  ;; otherwise if value put it otherwise delete it
+  (let ((existing (gethash key (nth 1 tbl))))
+    (if existing
+        (if value
+            (puthash key value (nth 1 tbl))
+          (remhash key (nth 1 tbl)))
+      (let ((meta (nth 2 tbl)))
+        (if meta (let ((nidx (ltbl-get '__newindex meta)))
+                   (funcall nidx tbl key value)))))))
+
+(defun ltbl-rawset (key value tbl)
   (if value
-      (puthash key value (nth 0 tbl))
-    (remhash key (nth 0 tbl))))
+      (puthash key value (nth 1 tbl))
+    (remhash key (nth 1 tbl))))
 
 (defun ltbl-get (key tbl)
-  (let ((val (gethash key (nth 0 tbl))))
-    (unless val
-      (ltbl-get '__index))))
+  ;; if something return that
+  ;; if nil check if __index is a latbl
+  ;; if so get from that and return
+  ;; if its a function then call it with table and key as params
+  (let ((val (gethash key (nth 1 tbl))))
+    (if val val
+      (let ((meta (nth 2 tbl)))
+        (if meta (let ((idx (ltbl-get '__index meta)))
+                   (if idx
+                       (if (ltblp idx)
+                           (ltbl-get key idx)
+                         (funcall idx tbl key))
+                     nil))
+          nil)))))
 
-;; if not there check metatable for __index method and call that
+(defun ltbl-rawget (key tbl)
+  (gethash key (nth 1 tbl)))
+
+(defun ltbl-call (key tbl &rest args)
+  ;; call a function stored in key in tbl with tbl and args
+  (apply (gethash key (nth 1 tbl)) tbl args))
 
 
 (defun ltbl-setmeta (tbl meta)
-  (setcdr tbl (list meta)))
+  (setcdr tbl (list (cadr tbl) meta)))
 
 (defun ltbl-getmeta (tbl)
-  (nth 1 tbl))
+  (nth 2 tbl))
