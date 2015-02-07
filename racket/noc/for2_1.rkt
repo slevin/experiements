27e7f904688d18c20d9d05fd9a4a823bd902fe39
Sean Levin <sean.levin@gmail.com>
2015-02-07 13:14:25 -0500

    object oriented implementation of mover

--- racket/noc/for2_1.rkt
+++ racket/noc/for2_1.rkt
@@ -16,92 +16,86 @@
     (super-new)))
 
 
+(define mover%
+  (class object%
+    (init [x 0] [y 0])
 
+    (define pos (vector x y))
+    (define vel (vector 0 0))
+    (define acc (vector 0 0))
 
+    (define/public (apply-force v)
+      (set! acc  (v+ acc v)))
 
-(class box%
-  (init [width 640] [height 320])
-
-  (define width width)
-  (define height height)
-  (define movers '())
-  (define frame #f)
-  (define canvas #f)
-  (define timer #f)
-
-  (super-new)
-
-  (define/public (new-mover)
-    ;; create mover in center and add to list of movers
-    (let ((m (new mover%
-                  [x (/ width 2)]
-                  [y (/ height 2)])))
-      (set! movers (cons m movers))))
-
-
-  (define (timer-tick)
-
-    (set! vel (v+ vel accel))
-    (set! pos (v+ pos vel))
-    (send canvas refresh-now)
-    )
-
-  (define/public (update)
-    (for ([m movers])
-      (let (mouse-accel (v* (vnormalize (v- mouse (send m get-pos))) 0.2)))
-      (send m apply-force )
+    (define/public (update)
+      (set! vel (v+ vel acc))
+      (set! acc (vector 0 0))
+      (set! pos (v+ pos vel))
       )
+    (define/public (get-pos) pos)
 
+    (define/public (paint-me dc)
+      (send dc draw-ellipse
+            (- (vector-ref pos 0) 40)
+            (- (vector-ref pos 1) 40) 80 80))
 
-    ;; apply current
-    )
+    (super-new)))
 
-  (define (paint-me dc)
-    (send dc set-pen "DarkRed" 3 'solid)
-    (send dc set-brush "Firebrick" 'solid)
-    (for ([m movers]) (send m paint-me dc)))
+(define box%
+  (class object%
+    (init [width 640] [height 320])
 
-  (define/public (show-scene)
-    (set! frame (new frame%
-                      [label "stuff"]
-                      [width width]
-                      [height height]))
+    (define _width width)
+    (define _height height)
+    (define movers '())
+    (define frame #f)
+    (define canvas #f)
+    (define timer #f)
 
-    (set! canvas (new my-canvas%
-                      [paint-callback (lambda (canvas dc) (paint-me dc))]
-                      [parent frame]))
-    (send frame show #t)
-    (define timer (new timer%
+    (super-new)
+
+    (define/public (new-mover)
+      ;; create mover in center and add to list of movers
+      (let ((m (new mover%
+                    [x (/ _width 2)]
+                    [y (/ _height 2)])))
+        (set! movers (cons m movers))))
+
+    (define/public (update)
+      (for ([m movers])
+        (let ((mouse-accel (v* (vnormalize (v- (send canvas get-mouse) (send m get-pos))) 0.2)))
+          (send m apply-force mouse-accel)
+          (send m update)))
+      (send canvas refresh-now))
+
+    (define (paint-me dc)
+      (send dc set-pen "DarkRed" 3 'solid)
+      (send dc set-brush "Firebrick" 'solid)
+      (for ([m movers]) (send m paint-me dc)))
+
+    (define/public (show-scene)
+      (set! frame (new frame%
+                       [label "stuff"]
+                       [width _width]
+                       [height _height]))
+
+      (set! canvas (new my-canvas%
+                        [paint-callback (lambda (canvas dc) (paint-me dc))]
+                        [parent frame]))
+      (send frame show #t)
+      (set! timer (new timer%
                        [interval 16]
-                       [notify-callback timer-tick])))
+                       [notify-callback (lambda () (update))])))
 
-  )
+    ))
 
-(class mover%
-  (init [x 0] [y 0])
-
-  (define pos (vector x y))
-  (define vel (vector 0 0))
-  (define acc (vector 0 0))
-
-  (define/public (apply-force v)
-    (set! acc  (v+ acc v)))
-
-  (define/public (get-pos) pos)
-
-  (define/public (paint-me dc)
-    (send dc draw-ellipse (- (vector-ref pos 0) 40) (- (vector-ref pos 1) 40) 80 80))
-
-  (super-new))
 
 
 (define bx (new box%))
 
 (send bx new-mover)
 
-;; how does it compare to their class?
-;; is it worth making a class for somee of these functions?
-
+(send bx show-scene)
 
 ;; gonna do the same thing but want to do something with gravity? fluids? some exercise in there?
 
