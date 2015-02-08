08a55885c388633702fdacf5d42511dc540c02e9
Sean Levin <sean.levin@gmail.com>
2015-02-07 19:21:13 -0500

    basic moving bouncing system in oop style

--- racket/noc/for2_1.rkt
+++ racket/noc/for2_1.rkt
@@ -18,14 +18,15 @@
 
 (define mover%
   (class object%
-    (init [x 0] [y 0])
+    (init [x 0] [y 0] [mass 10])
 
     (define pos (vector x y))
     (define vel (vector 0 0))
     (define acc (vector 0 0))
-
+    (define _mass mass)
+    
     (define/public (apply-force v)
-      (set! acc  (v+ acc v)))
+      (set! acc  (v+ acc (v/ v _mass))))
 
     (define/public (update)
       (set! vel (v+ vel acc))
@@ -34,10 +35,28 @@
       )
     (define/public (get-pos) pos)
 
+    (define (get-radius) (* _mass 4))
+    
+    (define (bounce-it pos-index max)
+      (if (or (and (> 0 (- (vector-ref pos pos-index) (get-radius)))
+                   (> 0 (vector-ref vel pos-index))
+                   )
+              (and (< max (+ (vector-ref pos pos-index) (get-radius)))
+                   (< 0 (vector-ref vel pos-index))
+                   ))
+          (vector-set! vel pos-index (* -1 (vector-ref vel pos-index)))
+          #f))
+    
+    (define/public (enforce-bounds max-x max-y)
+      (bounce-it 0 max-x)
+      (bounce-it 1 max-y))
+
     (define/public (paint-me dc)
-      (send dc draw-ellipse
-            (- (vector-ref pos 0) 40)
-            (- (vector-ref pos 1) 40) 80 80))
+      (let* ((radius (get-radius))
+             (size (* radius 2)))
+        (send dc draw-ellipse
+              (- (vector-ref pos 0) radius)
+              (- (vector-ref pos 1) radius) size size)))
 
     (super-new)))
 
@@ -54,18 +73,23 @@
 
     (super-new)
 
-    (define/public (new-mover)
+    (define/public (new-mover mass)
       ;; create mover in center and add to list of movers
       (let ((m (new mover%
                     [x (/ _width 2)]
-                    [y (/ _height 2)])))
+                    [y (/ _height 2)]
+                    [mass mass])))
         (set! movers (cons m movers))))
 
     (define/public (update)
       (for ([m movers])
-        (let ((mouse-accel (v* (vnormalize (v- (send canvas get-mouse) (send m get-pos))) 0.2)))
-          (send m apply-force mouse-accel)
-          (send m update)))
+        (let (
+              ;;(mouse-accel (v* (vnormalize (v- (send canvas get-mouse) (send m get-pos))) 0.2))
+              )
+          ;;(send m apply-force mouse-accel)
+          (send m apply-force (vector 0.1 3))
+          (send m update)
+          (send m enforce-bounds _width _height)))
       (send canvas refresh-now))
 
     (define (paint-me dc)
@@ -93,10 +117,9 @@
 
 (define bx (new box%))
 
-(send bx new-mover)
+(send bx new-mover 5)
 
+(send bx new-mover 10)
+(send bx new-mover 2)
+(send bx new-mover 14)
 (send bx show-scene)
-
-;; gonna do the same thing but want to do something with gravity? fluids? some exercise in there?
-
-;; figure out typed racket and possibly classes
