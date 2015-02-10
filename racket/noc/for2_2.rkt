a2f02e913d0bf11732c41ff7253cfc8edcca6665
Sean Levin <slevin@yahoo-inc.com>
2015-02-10 18:11:15 -0500

    got something showing with typed racket

--- racket/noc/for2_2.rkt
+++ racket/noc/for2_2.rkt
@@ -3,13 +3,15 @@
 (require racket/flonum)
 
 (require/typed "for2extra.rkt"
-               [gui-scene (-> Flonum Flonum Any)])
+               [gui-scene (-> Integer Integer Any)])
 
 (struct: scene ([size : FlVector]))
 
 
 (: make-scene (-> Flonum Flonum scene))
 (define (make-scene width height)
-  (gui-scene width height)
+  (gui-scene (fl->exact-integer width) (fl->exact-integer height))
   (scene (flvector width height))
   )
+
+(make-scene 640.0 320.0)
