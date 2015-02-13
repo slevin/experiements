a3e388c1e371c1f2903226efd45aa02f5d3468db
Sean Levin <slevin@yahoo-inc.com>
2015-02-12 21:37:10 -0500

    sort of drawing
    getting things passed around from typed to not working

--- racket/noc/for2_2.rkt
+++ racket/noc/for2_2.rkt
@@ -3,22 +3,27 @@
 (require racket/flonum)
 
 (require/typed "for2extra.rkt"
-               [scene-context (-> Integer Integer Any)])
+               [#:opaque Scene-context scene-context?]
+               [make-scene-context (-> Integer Integer Scene-context)]
+               [draw-circle (-> Scene-context Flonum Flonum Flonum Void)]
+               [render-scene (-> Scene-context Void)])
 
 (struct: scene ([size : FlVector]
-                [context : Any]))
+                [context : Scene-context]))
 
 
 (: make-scene (-> Flonum Flonum scene))
 (define (make-scene width height)
   (scene (flvector width height)
-         (scene-context (fl->exact-integer width) (fl->exact-integer height))
+         (make-scene-context (fl->exact-integer width) (fl->exact-integer height))
          )
   )
 
 
-(make-scene 640.0 320.0)
-
+(define sc (make-scene 640.0 320.0))
+(draw-circle (scene-context sc) 40. 40. 40.)
+(draw-circle (scene-context sc) 40. 40. 60.)
+(render-scene (scene-context sc))
 
 #|
 
--- racket/noc/for2extra.rkt
+++ racket/noc/for2extra.rkt
@@ -3,7 +3,10 @@
 (require racket/gui)
 (require racket/draw)
 
-(provide make-scene-context)
+(provide make-scene-context
+         render-scene
+         draw-circle
+         scene-context?)
 
 (struct scene-context (frame canvas [commands #:mutable]))
 
@@ -12,22 +15,37 @@
                      [label "stuff"]
                      [width width]
                      [height height]))
-        (cv (new canvas%
-                      [paint-callback (lambda (canvas dc) #f)]
-                      [parent fr]))
+        (cv (new canvas% [parent fr]))
         (ctx (scene-context fr cv '())))
     (send fr show #t)
     ctx))
 
-(define (render-scene 
-(define (draw-circle context x y radius)
-  (set-scene-context-commands! context
-                               (cons
-                                (lambda (dc)
-                                  (let ((size (* radius 2)))
-                                    (send dc draw-ellipse (- x radius) 
-                                          (- y radius) size size)))
-                                (scene-context-commands context))
-                                ))
 
-  
+(define (render-scene context)
+  (send (scene-context-canvas context)
+        refresh-now
+        (lambda (dc)
+          (for ([c (scene-context-commands context)])
+            (c dc)))))
+
+(define (add-draw-command context command)
+  (set-scene-context-commands! context 
+                               (cons command 
+                                     (scene-context-commands context))))
+
+(define (draw-circle context x y radius)
+  (add-draw-command context
+                    (lambda (dc)
+                      (let ((size (* radius 2))
+                            (new-x (- x radius))
+                            (new-y (- y radius)))
+                        (send dc draw-ellipse new-x new-y size size)))))
+
+#|
+
+set colors
+
+some notion of a timer that's firing off each second
+
+
+|#
