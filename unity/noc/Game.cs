a1149251e0413faa698b04f588dc44dc84ffcc39
Sean Levin <sean.levin@gmail.com>
2015-02-20 09:09:12 -0500

    implement bouncing ball room

--- unity/noc/Game.cs
+++ unity/noc/Game.cs
@@ -2,6 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[System.Serializable]
+public class Bounds {
+	public float minX;
+	public float maxX;
+	public float minZ;
+	public float maxZ;
+}
 public class Game : MonoBehaviour {
 
 	public GameObject prototype;
@@ -9,20 +16,44 @@ public class Game : MonoBehaviour {
 
 	public int minBalls;
 	public int maxBalls;
+	public float minSize;
+	public float maxSize;
+	public Bounds bounds;
 
-	private List<GameObject> balls;
+	private List<Sphere> balls;
+	private List<Vector3> forces;
 
 	// Use this for initialization
 	void Start () {
-		this.balls = new List<GameObject> ();
-		int count = Mathf.RoundToInt( Random.Range (minBalls, maxBalls));
-		for (int i = 0; i < count; i++) {
-			this.balls.Add (Instantiate (prototype, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject);
-		}
+		forces = new List<Vector3> ();
+		forces.Add (new Vector3 (0.5f, 0f, 0f));
+		forces.Add (new Vector3 (0f, 0f, -0.1f));
 
-		// position ball somewhere on the board
+		this.balls = new List<Sphere> ();
+		int count = IntRange (minBalls, maxBalls);
+		for (int i = 0; i < count; i++) {
+			float xPos = Random.Range(bounds.minX, bounds.maxX);
+			float zPos = Random.Range(bounds.minZ, bounds.maxZ);
+			float size = Random.Range (minSize, maxSize);
+			GameObject obj = Instantiate (prototype, new Vector3 (xPos, size/2.0f, zPos), Quaternion.identity) as GameObject; 
+			obj.transform.localScale = new Vector3(size, size, size);
+			Sphere sp = obj.GetComponent<Sphere>();
+			sp.size = size;
+			sp.bounds = bounds;
+			this.balls.Add (sp);
+		}
 	}
 
+
+	int IntRange(int min, int max) {
+		return Mathf.RoundToInt (Random.Range (min, max));
+	}
+
+	void FixedUpdate() {
+		foreach (Sphere sp in this.balls) {
+			sp.ApplyForces(forces);
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
--- unity/noc/sphere.cs
+++ unity/noc/sphere.cs
@@ -1,18 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class sphere : MonoBehaviour {
+public class Sphere : MonoBehaviour {
 
-	public int minSize;
-	public int maxSize;
+	public float size;
+	public Bounds bounds;
+
+	private Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	public void ApplyForces(List<Vector3> forces) {
+		foreach (Vector3 f in forces) {
+			this.velocity = this.velocity + f;
+		}
+		transform.position = transform.position + (velocity * Time.deltaTime / size);
+
+		if ((transform.position.x < this.bounds.minX && this.velocity.x < 0) ||
+		    (transform.position.x > this.bounds.maxX && this.velocity.x > 0)) {
+			velocity = new Vector3(-this.velocity.x, this.velocity.y, this.velocity.z);
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x, bounds.minX, bounds.maxX),
+			                                 transform.position.y,
+			                                 transform.position.z);
+		}
+		if ((transform.position.z < this.bounds.minZ && this.velocity.z < 0) ||
+			(transform.position.z > this.bounds.maxZ && this.velocity.z > 0)) {
+			velocity = new Vector3(this.velocity.x, this.velocity.y, -this.velocity.z);
+			transform.position = new Vector3(transform.position.x,
+			                                   transform.position.y,
+			                                   Mathf.Clamp(transform.position.z, bounds.minZ, bounds.maxZ));
+		}	
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		 
 	}
 }
