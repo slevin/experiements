b87e76f5eea61da51ab25ecb603174cc27479898
Sean Levin <sean.levin@gmail.com>
2015-02-22 13:54:19 -0500

    add some sound and waves

--- unity/shooter/PlayerController.cs
+++ unity/shooter/PlayerController.cs
@@ -25,7 +25,8 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
-	}
+			audio.Play();
+		}
 	}
 
 	void FixedUpdate() {
