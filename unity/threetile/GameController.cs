c62d0482eadd2d42931d8b0d7f5bb85c2655c3a5
Sean Levin <sean.levin@gmail.com>
2015-02-28 08:26:34 -0500

    tiles are showing

--- unity/threetile/GameController.cs
+++ unity/threetile/GameController.cs
@@ -61,11 +61,14 @@ public class Slot : Object
 {
     private IntPair pos;
     private Dictionary<IntPair, Slot> allSlots;
+    private Vector3 location;
+    private Tile tile;
 
-    public Slot(IntPair pos, Dictionary<IntPair, Slot> allSlots)
+    public Slot(IntPair pos, Dictionary<IntPair, Slot> allSlots, Vector3 location)
     {
         this.pos = pos;
         this.allSlots = allSlots;
+        this.location = location;
     }
 
     private Slot otherSlot(IntPair pos)
@@ -94,14 +97,15 @@ public class Slot : Object
     }
 }
 
-public class Tile
+public class Tile : Object
 {
-    GameObject tilePrefab;
+//    GameObject tilePrefab;
     GameObject tileObject;
 
-    public Tile(GameObject tilePrefab)
+    public Tile(GameObject tilePrefab, Vector3 location, Vector2 tileShift)
     {
-        this.tilePrefab = tilePrefab;
+        tileObject = Instantiate(tilePrefab, location, Quaternion.identity) as GameObject;
+        tileObject.renderer.material.mainTextureOffset = tileShift;
     }
 
     public void instantiateGameObject()
@@ -115,18 +119,25 @@ public class Board : Object
 //    private int columns;
 //    private int rows;
 
-    public Board(int columns, int rows, GameObject tileQuad)
+    public Board(int columns, int rows, GameController controller)
     {
 //        this.columns = columns;
 //        this.rows = rows;
 
         int count = rows * columns;
-        Dictionary<IntPair, Slot> allSlots = new Dictionary<IntPair, Slot> (count, new IntPairComparer ());
+        Dictionary<IntPair, Slot> allSlots = new Dictionary<IntPair, Slot> (count, new IntPairComparer());
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < columns; x++) {
                 IntPair pair = new IntPair (x, y); 
-                Slot slot = new Slot (pair, allSlots);
+                Vector3 location = controller.SlotLocation(pair);
+                Slot slot = new Slot (pair, allSlots, location);
                 allSlots.Add (pair, slot);
+
+                bool lastOne = x == columns - 1 && y == rows - 1;
+                if (!lastOne) {
+                    //Tile t = new Tile(controller.tilePrefab, location, controller.TileShift(pair));
+                    GameObject tobj = controller.CreateTile(pair);
+                }
             }
         }
     }
@@ -150,29 +161,9 @@ public class GameController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        
-        //Board board = new Board(2, 2);
-
-        /*
-         * board draw yourself
-         * for each item in 
-*/
-
-
-        /*
-        GameObject topLeft = Instantiate (tileQuad, new Vector3 (-1.1f, 1.1f), Quaternion.identity) as GameObject;
-        topLeft.renderer.material.mainTextureOffset = new Vector2 (0f, 0.66f);
-        GameObject topMiddle = Instantiate (tileQuad, new Vector3 (0f, 1.1f), Quaternion.identity) as GameObject;
-        topMiddle.renderer.material.mainTextureOffset = new Vector2 (0.33f, 0.66f);
-        GameObject topRight = Instantiate (tileQuad, new Vector3 (1.1f, 1.1f), Quaternion.identity) as GameObject;
-        topRight.renderer.material.mainTextureOffset = new Vector2 (0.66f, 0.66f);
-
-        GameObject left = Instantiate (tileQuad, new Vector3 (-1.1f, 0f), Quaternion.identity) as GameObject;
-        left.renderer.material.mainTextureOffset = new Vector2 (0f, 0.33f);
-*/
+         Board board = new Board(columns, rows, this);
 
     }
-
 
     // Update is called once per frame
     void Update()
@@ -180,22 +171,30 @@ public class GameController : MonoBehaviour
 
     }
 
-    public GameObject CreateTile(int positionX, int positionY)
+    public Vector3 SlotLocation(IntPair pos)
     {
         float totalWidth = this.columns * this.tileSize + (this.columns - 1) * this.tileSpacing;
         float totalHeight = this.rows * this.tileSize + (this.rows - 1) * this.tileSpacing;
         float halfWidth = totalWidth / 2.0f;
         float halfHeight = totalHeight / 2.0f;
-        float myX = this.tileSize + this.tileSpacing * positionX;
-        float myY = this.tileSize * this.tileSpacing * positionY;
-
+        float myX = this.tileSize + this.tileSpacing * pos.x;
+        float myY = this.tileSize * this.tileSpacing * pos.y;
         
         float p6nOffX = myX - halfWidth + (this.tileSize / 2.0f);
         float p6nOffY = myY - halfHeight + (this.tileSize / 2.0f);
-        GameObject obj = Instantiate (tilePrefab, 
-                                      new Vector3 (p6nOffX, p6nOffY), 
-                                      Quaternion.identity) as GameObject;
-        obj.renderer.material.mainTextureOffset = new Vector2(0f, 0.66f);
+        return new Vector3(p6nOffX, p6nOffY);
+    }
+
+    public Vector2 TileShift(IntPair pos) {
+        float xTileShift = pos.x / (float)this.columns;
+        float yTileShift = (this.rows - (pos.y + 1)) / (float)this.rows;
+        return new Vector2(xTileShift, yTileShift);
+    }
+
+    public GameObject CreateTile(IntPair pos)
+    {
+        GameObject obj = Instantiate(this.tilePrefab, SlotLocation(pos), Quaternion.identity) as GameObject;
+        obj.renderer.material.mainTextureOffset = TileShift(pos);
         return obj;
     }
 }
