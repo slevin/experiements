2fca1b6bff10df866102f853510139812d6e6e18
Sean Levin <sean.levin@gmail.com>
2015-03-06 20:50:57 -0500

    adding renderer

--- go/sdl2test/try.go
+++ go/sdl2test/try.go
@@ -3,12 +3,26 @@ package main
 import "github.com/veandco/go-sdl2/sdl"
 
 func main() {
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+
+	err := sdl.Init(sdl.INIT_VIDEO)
+	if err != nil {
+		panic(err)
+	}
+
+	window, err := sdl.CreateWindow("Here's a Screen", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		800, 600, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
 	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED | sdl.RENDERER_PRESENTVSYNC)
+	if renderer == nil {
+		panic(err)
+	}
+	defer renderer.Destroy()
+
+
 
 	surface := window.GetSurface()
 
