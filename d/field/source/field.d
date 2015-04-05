28abe237e6913e4c7531a01a18f75f88025c839e
Sean Levin <sean.levin@gmail.com>
2015-04-05 14:58:54 -0400

    tests for field generation

--- d/field/source/field.d
+++ d/field/source/field.d
@@ -15,8 +15,9 @@ struct Field {
     enum size_t rows = 10;
     enum int width = 20;
     enum int height = 20;
+    enum float lineLength = 14;
     vec2 slopes[cols][rows];
-    sfVertex[cols * rows] lineVertices;
+    sfVertex[cols * rows * 2] lineVertices; // 2 points per line
 
     vec2 flowVectorForPosition(vec2 pos) {
         size_t x = cast(size_t)floor(pos.x / width);
@@ -29,14 +30,28 @@ struct Field {
     }
 
     void fill(float function(ulong, ulong) fillFunction) {
-        // takes the slopes and creates a line that matches the slope
-        // with the proper offset
         foreach(y; 0 .. rows) {
             foreach(x; 0 .. cols) {
-                slopes[x][y] = unitAngleToVec(fillFunction(x, y));
-                lineVertices[x + y * rows] = sfVertex(sfVector2f(0,0), sfRed);
+                vec2 slope = unitAngleToVec(fillFunction(x, y));
+                slopes[x][y] = slope;
+                // center around 0
+                vec2 v1 = vec2(slope.x * lineLength / 2.0f,
+                               slope.y * lineLength / 2.0f);
+                vec2 v2 = v1 * -1;
+                // move to center of matching square
+                vec2 offset = boxCenter(x, y);
+                v1 += offset;
+                v2 += offset;
+                sfVertex vtx1 = sfVertex(sfVector2f(v1.x, v1.y), sfRed);
+                sfVertex vtx2 = sfVertex(sfVector2f(v2.x, v2.y), sfRed);
+                lineVertices[(x + y * rows) * 2] = vtx1;
+                lineVertices[(x + y * rows) * 2 + 1] = vtx2;
             }
         }
+    }
+
+    vec2 boxCenter(ulong x, ulong y) {
+        return vec2(x * width + (width / 2.0f), y * height + (height / 2.0f));
     }
 
     void render(sfRenderWindow *win) {
@@ -87,26 +102,35 @@ unittest {
     assert(dir == field.slopes[0][1]);
 
 
+    // drawable lines
 
-    // what is a line?
+    assert(field.boxCenter(0, 0) == vec2(10, 10));
+    assert(field.boxCenter(1, 0) == vec2(30, 10));
+    assert(field.boxCenter(1, 1) == vec2(30, 30));
+
+    // should have rows * cols * 2 (2 pts per line)
+    assert(200 == field.numberOfLines());
+
+    sfVector2f v1;
+    sfVector2f v2;
+
+    // first line
+    v1 = field.lineVertices[0].position;
+    v2 = field.lineVertices[1].position;
+    assert(v1.x == 17 && v1.y == 10);
+    assert(v2.x ==  3 && v2.y == 10);
+
+
     /*
+should 1 == 0 seems like there will be a harsh line at that point
+maybe should go from 0 fully rotated to 0 again something to think about
 
-    // should have rows * cols lines
-    assert(100 == field.numberOfLines());
-    */
-    // line
-
-    /*
-        sfVertex[] line1 = [sfVertex(sfVector2f(pos.x - sz, pos.y), color),
-                            sfVertex(sfVector2f(pos.x + sz, pos.y), color)];
-        sfVertex[] line2 = [sfVertex(sfVector2f(pos.x, pos.y - sz), color),
-                            sfVertex(sfVector2f(pos.x, pos.y + sz), color)];
-
-        sfRenderWindow_drawPrimitives(win, line1.ptr, 2, sfLines, null);
-        sfRenderWindow_drawPrimitives(win, line2.ptr, 2, sfLines, null);
+could go out of range with some of these queries
+need to catch that? or with position queries
     */
 
 }
+
 
 vec2 unitAngleToVec(float angle)
     in { assert(angle >= 0 && angle <= 1.0); }
@@ -115,25 +139,25 @@ vec2 unitAngleToVec(float angle)
         // into a unit vector
 
         auto theta = 2 * PI * angle;
-        auto x = round(sin(theta) * 100) / 100;
-        auto y = round(cos(theta) * 100) / 100;
+        auto x = round(cos(theta) * 100) / 100;
+        auto y = round(sin(theta) * 100) / 100;
         return vec2(x, y);
     }
 
 unittest {
     vec2 v;
     v = unitAngleToVec(0);
-    assert(v == vec2(0, 1));
-
-    v = unitAngleToVec(0.25);
     assert(v == vec2(1, 0));
 
-    v = unitAngleToVec(0.5);
-    assert(v == vec2(0, -1));
+    v = unitAngleToVec(0.25);
+    assert(v == vec2(0, 1));
 
-    v = unitAngleToVec(0.75);
+    v = unitAngleToVec(0.5);
     assert(v == vec2(-1, 0));
 
+    v = unitAngleToVec(0.75);
+    assert(v == vec2(0, -1));
+
     v = unitAngleToVec(1.0);
-    assert(v == vec2(0, 1));
+    assert(v == vec2(1, 0));
 }
