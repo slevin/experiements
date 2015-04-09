5e5bca8a6129e84ded3692650cedee2d7fb6be3c
Sean Levin <sean.levin@gmail.com>
2015-04-08 20:01:47 -0400

    start path for path following

---
+++ d/field/source/path.d
@@ -0,0 +1,15 @@
+module path;
+
+import std.math;
+import std.stdio;
+import std.format;
+
+import derelict.sfml2.graphics;
+import derelict.sfml2.system;
+
+import gl3n.linalg;
+
+struct Path(size_t points) {
+
+
+}
