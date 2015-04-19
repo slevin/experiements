1a059b7e5179189127b38dfd1679d89e0ec688b0
Sean Levin <sean.levin@gmail.com>
2015-04-19 09:48:07 -0400

    figured out that static arrays anen't ranges

--- d/field/try.d
+++ d/field/try.d
@@ -20,12 +20,23 @@ struct TryIt {
     }
 }
 
+int addSome(TryIt t, int num) {
+    return t.a + num;
+}
+
 void main() {
     TryIt[5] t;
+    int[5] vals;
 
     auto param = 5;
 
-    int[5] res = map!(delegate int(TryIt p) { return p.addTo(param); })(t);
+    //int[5] res = map!(delegate int(TryIt p) { return p.addTo(param); })(t);
     //auto res = map!(a => a.addTo(param))(t);
+    //auto res = map!(a => a.addTo(5))(t);
+    //auto res = map!(a => addSome(a, 5))(t);
+    int[] arr1 = [1,2,3,4,5];
+    int[5] arr2 = [1,2,3,4,5];
+    //auto res = map!((a => a + 5))(vals);
+    auto squares = map!(a => a * a)(arr2[]);
 
 }
