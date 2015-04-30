4ab6a8c5d2c9a63399c5762c168d1e194a6b2bb2
Sean Levin <sean.levin@gmail.com>
2015-04-29 21:19:38 -0400

    more fiddling to figure compile time

--- d/field/fiddle.d
+++ d/field/fiddle.d
@@ -1,8 +1,13 @@
-#!/usr/local/bin/rdmd -unittest -I/Users/slevin/.dub/packages/unit-threaded-0.4.7/source/ -Isource -I/Users/slevin/.dub/packages/derelict-sfml2-1.1.0/source/ -I/Users/slevin/.dub/packages/derelict-util-1.9.1/source/ -I/Users/slevin/.dub/packages/gl3n-1.0.1
+#!/usr/local/bin/rdmd -I/Users/slevin/.dub/packages/gl3n-1.0.1
 
-import field2;
+import gl3n.linalg;
 
 void main(string[] args) {
+    enum size_t sz = 100000;
+    vec2 slopes[sz];
+    //Field!(sz) fld;
+}
 
-    Field2!(100, 100, 100, 100) fld;
+struct Field(size_t cols) {
+    vec2 slopes[cols];
 }
