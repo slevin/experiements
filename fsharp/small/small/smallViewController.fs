cf2f311950dda2f479b84099083f8e07c01be890
Sean Levin <sean.levin@gmail.com>
2015-06-24 21:41:46 -0400

    trying to figure out magic formula to get working

--- fsharp/small/small/smallViewController.fs
+++ fsharp/small/small/smallViewController.fs
@@ -4,12 +4,27 @@ open System
 open System.Drawing
 open MonoTouch.UIKit
 open MonoTouch.Foundation
+open MonoTouch.CoreGraphics
+
+[<Register("SnakeView")>]
+type SnakeView() =
+    inherit UIView()
+
+    override this.Draw(rect) =
+        base.Draw(rect)
+        let ctx = UIGraphics.GetCurrentContext()
+        UIColor.Red.SetFill()
+        ctx.FillRect(rect)
+
 
 [<Register("smallViewController")>]
 type smallViewController() = 
     inherit UIViewController()
+
     // Release any cached data, images, etc that aren't in use.
-    override this.DidReceiveMemoryWarning() = base.DidReceiveMemoryWarning()
+    override this.DidReceiveMemoryWarning() = 
+        base.DidReceiveMemoryWarning()
+    
     // Perform any additional setup after loading the view, typically from a nib.
     override this.ViewDidLoad() = 
         base.ViewDidLoad()
@@ -17,7 +32,12 @@ type smallViewController() =
         let im = new UIImage("ziggy.jpg")
         iv.Image <- im
         this.View.AddSubview(iv)
+        let pt = CoreGraphics.CGPoint(this.View.Bounds.X + 10, this.View.Bounds.Y + 10)
+        let sz = CGSize(CGPoint(this.View.Bounds.Width - 20, this.View.Bounds.Height - 20))
+        let less = CGRect(pt, sz)
+        let sv = new SnakeView(less)
+        this.View.AddSubview(sv)
+
     // Return true for supported orientations
     override this.ShouldAutorotateToInterfaceOrientation(orientation) = 
         orientation <> UIInterfaceOrientation.PortraitUpsideDown
-
