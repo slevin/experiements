631dfd73fa5b36fc4425958842ed015e61fe8851
Sean Levin <slevin@yahoo-inc.com>
2015-09-25 15:50:49 -0400

    keys control movement

--- haskell/snakehs/src/Main.hs
+++ haskell/snakehs/src/Main.hs
@@ -8,9 +8,27 @@ import SDL (($=))
 import qualified SDL
 import Data.IORef
 import Data.Word
+import Data.Maybe
 
--- data GameState = GameState {
---                            }
+
+data Direction = L | U | R | D
+               deriving(Show)
+
+-- turn an event into a possible direction if its an arrow key
+--key2Code :: SDL.EventPayload -> Maybe Int32
+key2Code (SDL.KeyboardEvent ked) = Just $ SDL.unwrapScancode $ SDL.keysymScancode $ SDL.keyboardEventKeysym ked
+key2Code _ = Nothing
+
+key2Direction :: SDL.EventPayload -> Maybe Direction
+key2Direction (SDL.KeyboardEvent ked)
+  | sym == 80 = Just L
+  | sym == 82 = Just U
+  | sym == 79 = Just R
+  | sym == 81 = Just D
+  where sym = SDL.unwrapScancode $ SDL.keysymScancode $ SDL.keyboardEventKeysym ked
+key2Direction _ = Nothing
+
+
 main :: IO ()
 main = do
   SDL.initialize [ SDL.InitEverything ]
@@ -25,7 +43,8 @@ main = do
   renderer <- SDL.createRenderer window (-1) rdrConfig
 
   lstRef <- newIORef (0 :: Word32)
-  offRef <- newIORef 0
+  offRef <- newIORef (0, 0)
+  dirRef <- newIORef R
 
   let loop = do
         let collectEvents = do
@@ -36,11 +55,22 @@ main = do
         events <- collectEvents
 
         let quit = any (== SDL.QuitEvent) $ map SDL.eventPayload events
+        let dirs = mapMaybe key2Direction $ map SDL.eventPayload events
+
+        forM_ dirs $ writeIORef dirRef
+        currentDir <- readIORef dirRef
+
+        -- putStrLn $ show dirs -- $ map key2Code $ map SDL.eventPayload events
+
         lst <- readIORef lstRef
         ts <- SDL.ticks
         if ts > lst + 400
           then do writeIORef lstRef ts
-                  modifyIORef offRef (\x -> x + 50)
+                  modifyIORef offRef (\x -> case currentDir of
+                                        L -> (fst x - 50, snd x)
+                                        R -> (fst x + 50, snd x)
+                                        U -> (fst x, snd x - 50)
+                                        D -> (fst x, snd x + 50))
           else do return ()
 
         SDL.rendererDrawColor renderer $= V4 0 0 0 255
@@ -48,7 +78,7 @@ main = do
 
         SDL.rendererDrawColor renderer $= V4 248 231 28 255
         off <- readIORef offRef
-        SDL.fillRect renderer $ Just $ SDL.Rectangle (P (V2 (100 + off) 100)) (V2 50 50)
+        SDL.fillRect renderer $ Just $ SDL.Rectangle (P (V2 (100 + fst off) (100 + snd off))) (V2 50 50)
         SDL.present renderer
 
         unless quit loop
